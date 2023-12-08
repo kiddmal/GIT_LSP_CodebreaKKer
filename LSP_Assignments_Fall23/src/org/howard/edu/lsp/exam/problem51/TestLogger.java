3a7f8d944d484988bce9a0dc745cc4eafcf36db6
kiddmal <78062499+kiddmal@users.noreply.github.com>
2023-12-07 20:33:27 -0500

    TestLogger.java

--- LSP_Assignments_Fall23/src/org/howard/edu/lsp/exam/problem51/TestLogger.java
+++ LSP_Assignments_Fall23/src/org/howard/edu/lsp/exam/problem51/TestLogger.java
@@ -11,15 +11,20 @@ import org.junit.jupiter.api.Test;
 public class TestLogger {
 	private Logger logger;
 
+	@BeforeEach
 	void setUp() {
 		logger = Logger.getInstance();
 	}
-	
+
+	@Test
+ 	@DisplayName("Test case for showing the log")
  	public void testDisplayLog() {
  		logger.logText("Hello World!");
  		assertEquals("Log:\nHello World!\n", logger.displayLog());  
  	}
 
+	@Test
+ 	@DisplayName("Test case for instances")
  	public void testLoggerInstances() {
  		Logger nextLogger = Logger.getInstance();
         assertSame(logger, nextLogger);
